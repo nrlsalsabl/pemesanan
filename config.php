99918085b1cf77a9898d184cbe4c0f73c193da12
nrlsalsabl <99568259+nrlsalsabl@users.noreply.github.com>
2024-08-13 10:43:33 +0700

    test update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "pemesanan";
+$database = "pemesanan"; 
 
 $conn = new mysqli($servername, $username, $password, $database);
 
--- login_process.php
+++ login_process.php
@@ -4,7 +4,7 @@ include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $password = $_POST['password']; 
 
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = $conn->query($query);
